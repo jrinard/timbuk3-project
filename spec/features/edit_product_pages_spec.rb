8f8b96d62437590fefc56a8d0c0ce00191bc1775
Joshua Rinard <jrinard@hotmail.com>
2017-02-10 09:03:40 -0800

    adds specs for edit and delete

---
+++ spec/features/edit_product_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "EDIT PRODUCT" do
+  it "edit product" do
+    visit products_path
+    click_link 'New Product'
+    fill_in 'name', :with => 'Backpack'
+    fill_in 'description', :with => 'Backpack'
+    fill_in 'origin', :with => 'Backpack'
+    fill_in 'cost', :with => 'Backpack'
+    fill_in 'style', :with => 'Backpack'
+    fill_in 'image', :with => 'url'
+    click_on 'Create Product'
+    click_on 'Backpack'
+    click_on 'Edit Product'
+    fill_in 'name', :with => 'Backpack2'
+    fill_in 'description', :with => 'Backpack2'
+    fill_in 'origin', :with => 'Backpack2'
+    fill_in 'cost', :with => 'Backpack2'
+    fill_in 'style', :with => 'Backpack2'
+    fill_in 'image', :with => 'url'
+    click_on 'Update Product'
+    expect(page).to have_content 'Backpack2'
+  end
+end
---
+++ spec/features/remove_product_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "REMOVE PRODUCT" do
+  it "removes product" do
+    visit products_path
+    click_link 'New Product'
+    fill_in 'name', :with => 'Backpack'
+    fill_in 'description', :with => 'Backpack'
+    fill_in 'origin', :with => 'Backpack'
+    fill_in 'cost', :with => 'Backpack'
+    fill_in 'style', :with => 'Backpack'
+    fill_in 'image', :with => 'url'
+    click_on 'Create Product'
+    click_on 'Backpack'
+    click_on 'Remove Product'
+    expect(page).to_not have_content 'Backpack'
+  end
+end
